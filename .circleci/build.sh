7a85819b6dcebd84d372ed1a69446aec0e57815b
Pierre Grandin <grandinp@gmail.com>
2017-09-18 23:42:54 -0700

    Use env vars for target and stage

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,4 @@
-stage=$1
-
 sha1=`git log --format=format:%H -1 ${stage}/`
-docker pull pgrandin/rpi0-buildroot:${sha1} &&
-   docker tag pgrandin/rpi0-buildroot:${sha1} pgrandin/rpi0-buildroot:${stage} ||
-   docker build -t pgrandin/rpi0-buildroot:${stage} ${stage}
+docker pull pgrandin/${target}-buildroot:${sha1} &&
+   docker tag pgrandin/${target}-buildroot:${sha1} pgrandin/${target}-buildroot:${stage} ||
+   docker build -t pgrandin/${target}-buildroot:${stage} ${stage}
--- .circleci/push.sh
+++ .circleci/push.sh
@@ -1,12 +1,10 @@
-stage=$1
-
 sha1=`git log --format=format:%H -1 ${stage}/`
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker tag pgrandin/rpi0-buildroot:${stage} pgrandin/rpi0-buildroot:${sha1}
-docker push pgrandin/rpi0-buildroot:${stage}
-docker push pgrandin/rpi0-buildroot:${sha1}
+docker tag pgrandin/${target}-buildroot:${stage} pgrandin/${target}-buildroot:${sha1}
+docker push pgrandin/${target}-buildroot:${stage}
+docker push pgrandin/${target}-buildroot:${sha1}
 
-docker run pgrandin/rpi0-buildroot:${sha1} ls /usr/local/share/buildroot-2017.02.3/output/images/
+docker run pgrandin/${target}-buildroot:${sha1} ls /usr/local/share/buildroot-2017.02.3/output/images/
 container=`docker ps -a|grep ${sha1}|cut -f1 -d" "`
 [ -d /output ] || mkdir /output
 docker cp ${container}:/usr/local/share/buildroot-2017.02.3/output/images/ /output/${stage}
