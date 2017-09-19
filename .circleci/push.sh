6dbd6dba79aa4f308b6d2ec2f9d55eab88f60a04
Pierre Grandin <grandinp@gmail.com>
2017-09-18 23:27:52 -0700

    Better artifact management

--- .circleci/push.sh
+++ .circleci/push.sh
@@ -5,3 +5,8 @@ docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker tag pgrandin/rpi0-buildroot:${stage} pgrandin/rpi0-buildroot:${sha1}
 docker push pgrandin/rpi0-buildroot:${stage}
 docker push pgrandin/rpi0-buildroot:${sha1}
+
+docker run pgrandin/rpi0-buildroot:${sha1} ls /usr/local/share/buildroot-2017.02.3/output/images/
+container=`docker ps -a|grep ${sha1}|cut -f1 -d" "`
+[ -d /output ] || mkdir /output
+docker cp ${container}:/usr/local/share/buildroot-2017.02.3/output/images/ /output/${stage}
