44ebd36110355bcc10da1e86ec54b3fe2d0323ea
Pierre Grandin <grandinp@gmail.com>
2017-09-23 16:58:49 -0700

    Use correct Dockerfile for each target

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,4 +1,5 @@
 sha1=`git log --format=format:%H -1 ${stage}/`
 docker pull pgrandin/${target}-buildroot:${sha1} &&
    docker tag pgrandin/${target}-buildroot:${sha1} pgrandin/${target}-buildroot:${stage} ||
+   sed -i -e "s/%target/${target}/" ${stage}/Dockerfile &&
    docker build -t pgrandin/${target}-buildroot:${stage} ${stage}
