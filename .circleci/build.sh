869ecd5ee665ee0daef35cadb36a0799553f9223
Pierre Grandin <grandinp@gmail.com>
2017-09-23 17:04:34 -0700

    Fixed logic flaw

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,5 +1,5 @@
 sha1=`git log --format=format:%H -1 ${stage}/`
+sed -i -e "s/%target/${target}/" ${stage}/Dockerfile
 docker pull pgrandin/${target}-buildroot:${sha1} &&
    docker tag pgrandin/${target}-buildroot:${sha1} pgrandin/${target}-buildroot:${stage} ||
-   sed -i -e "s/%target/${target}/" ${stage}/Dockerfile &&
    docker build -t pgrandin/${target}-buildroot:${stage} ${stage}
