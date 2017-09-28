771e1ea521e6a2274c3421ed9e301147877780e8
Pierre Grandin <grandinp@gmail.com>
2017-09-28 12:41:15 -0700

    Always start with a clean Dockerfile

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,8 @@
 set -x
 set -e
 
+git checkout ${stage}/Dockerfile
+
 sha1=`git log --format=format:%H -1 ${stage}/`
 sed -i -e "s/%target%/${target}/" ${stage}/Dockerfile
 
@@ -10,7 +12,6 @@ if [[ "${from}" != "" ]]; then
     from_tag=`echo $from|cut -f2 -d':'`
     sha1_tag=`git log --format=format:%H -1 ${from_tag}/`
     new_from=`echo $from|sed -e "s/${from_tag}/${sha1_tag}/"`
-    git checkout ${stage}/Dockerfile
     sed -i -e "s@${from}@${new_from}@" ${stage}/Dockerfile
 fi
 
