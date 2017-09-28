ed7330ff6f1ea56b0bdc477f144557492e7e8060
Pierre Grandin <grandinp@gmail.com>
2017-09-28 15:08:14 -0700

    Use recursive md5 instead of git sha1 to tag images

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,18 +3,22 @@ set -e
 
 git checkout ${stage}/Dockerfile
 
-sha1=`git log --format=format:%H -1 ${stage}/`
+find ${stage} -type f | xargs md5sum > ${stage}.txt
+tag=`md5sum ${stage}.txt | cut -d' ' -f1`
+# tag=`git log --format=format:%H -1 ${stage}/`
 sed -i -e "s/%target%/${target}/" ${stage}/Dockerfile
 
-from=`grep "FROM pgrandin/" ${stage}/Dockerfile |cut -f2 -d ' '`
+echo "Going to build/check for pgrandin/${target}-buildroot:${tag}"
 
+from=`grep "FROM pgrandin/" ${stage}/Dockerfile |cut -f2 -d ' '`
 if [[ "${from}" != "" ]]; then
     from_tag=`echo $from|cut -f2 -d':'`
-    sha1_tag=`git log --format=format:%H -1 ${from_tag}/`
-    new_from=`echo $from|sed -e "s/${from_tag}/${sha1_tag}/"`
+    find ${from_tag} -type f | xargs md5sum > ${from_tag}.txt
+    from_md5=`md5sum ${from_tag}.txt | cut -d' ' -f1`
+    new_from=`echo $from|sed -e "s/${from_tag}/${from_md5}/"`
     sed -i -e "s@${from}@${new_from}@" ${stage}/Dockerfile
+    echo "Going to use '${new_from}' to build/check for pgrandin/${target}-buildroot:${tag}"
 fi
-
 
 if [[ "${stage}" == "baseimage" ]]; then
     cat toolchain/${target}-glibc_defconfig >> baseimage/baseimage_defconfig
@@ -28,6 +32,6 @@ elif [[ "${stage}" == "graphics-gtk" ]]; then
 fi
 
 set +e
-docker pull pgrandin/${target}-buildroot:${sha1} &&
-   docker tag pgrandin/${target}-buildroot:${sha1} pgrandin/${target}-buildroot:${stage} ||
+docker pull pgrandin/${target}-buildroot:${tag} &&
+   docker tag pgrandin/${target}-buildroot:${tag} pgrandin/${target}-buildroot:${stage} ||
    docker build -t pgrandin/${target}-buildroot:${stage} ${stage}
--- .circleci/push.sh
+++ .circleci/push.sh
@@ -1,10 +1,14 @@
-sha1=`git log --format=format:%H -1 ${stage}/`
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker tag pgrandin/${target}-buildroot:${stage} pgrandin/${target}-buildroot:${sha1}
-docker push pgrandin/${target}-buildroot:${stage}
-docker push pgrandin/${target}-buildroot:${sha1}
+tag=`git log --format=format:%H -1 ${stage}/`
+tag=`md5sum ${stage}.txt | cut -d' ' -f1`
 
-docker run pgrandin/${target}-buildroot:${sha1} ls /usr/local/share/buildroot-2017.02.3/output/images/
-container=`docker ps -a|grep ${sha1}|cut -f1 -d" "`
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker tag pgrandin/${target}-buildroot:${stage} pgrandin/${target}-buildroot:${tag}
+docker push pgrandin/${target}-buildroot:${stage}
+docker push pgrandin/${target}-buildroot:${tag}
+
+echo "pgrandin/${target}-buildroot:${tag} tagged as pgrandin/${target}-buildroot:${stage}"
+
+docker run pgrandin/${target}-buildroot:${tag} ls /usr/local/share/buildroot-2017.02.3/output/images/
+container=`docker ps -a|grep ${tag}|cut -f1 -d" "`
 [ -d /output ] || mkdir /output
 docker cp ${container}:/usr/local/share/buildroot-2017.02.3/output/images/ /output/${stage}
