a454db80750798bf5972ff3decff8d7cfd49ffc6
Pierre Grandin <grandinp@gmail.com>
2018-01-12 00:10:13 -0800

    Cleaned up the libspotify package

--- packages/libspotify/libspotify.mk
+++ packages/libspotify/libspotify.mk
@@ -12,22 +12,12 @@ LIBSPOTIFY_INSTALL_STAGING = YES
 
 LIB=libspotify.so
 
-# define LIBSPOTIFY_BUILD_CMDS
-# 	(cd $(@D); \
-# 	$(TARGET_CXX) -Wall -fPIC -fno-rtti  -c ArduiPi_OLED.cpp; \
-# 	$(TARGET_CXX) -Wall -fPIC -fno-rtti  -c Adafruit_GFX.cpp; \
-# 	$(TARGET_CC) -Wall -fPIC -c bcm2835.c; \
-# 	$(TARGET_CXX) -shared -Wl,-soname,$(LIBNAME) -o $(LIBNAME) ArduiPi_OLED.o Adafruit_GFX.o bcm2835.o; \
-# 	)
-# endef
-
 # ./output/build/libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release/lib/libspotify.so.12
 # ./output/build/libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release/lib/libspotify.so
 # ./output/build/libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release/lib/pkgconfig/libspotify.pc
 # ./output/build/libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release/lib/libspotify.so.12.1.103
 # ./output/build/libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release/include/libspotify
 # ./output/build/libspotify-12.1.103-Linux-armv6-bcm2708hardfp-release/include/libspotify/api.h
-
 
 define LIBSPOTIFY_INSTALL_STAGING_CMDS
         cp $(@D)/lib/pkgconfig/libspotify.pc $(STAGING_DIR)/usr/lib/pkgconfig/
@@ -45,5 +35,3 @@ endef
 
 
 $(eval $(generic-package))
-# $(eval $(host-generic-package))
-
