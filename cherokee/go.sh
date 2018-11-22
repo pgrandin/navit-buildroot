bd470a535326ea811416738616bd09c2874b1844
Pierre Grandin <grandinp@gmail.com>
2018-11-22 15:37:27 -0800

    Splitting navit's config

--- cherokee/go.sh
+++ cherokee/go.sh
@@ -1,7 +1,14 @@
+set -e
+
+if [ -z ${target+x} ]; then
+	echo "You must set the target via an environment variable"
+	exit -1
+fi
 unset PERL_MM_OPT
 cat configs/${target}_defconfig \
 	../../glibc_defconfig \
-	../../cherokee_defconfig > configs/cherokee-${target}_defconfig
+	../../cherokee_defconfig \
+	../../navit_defconfig > configs/cherokee-${target}_defconfig
 echo "BR2_ROOTFS_POST_IMAGE_SCRIPT=\"../../custom-post-image.sh board/${target}/post-image.sh\"" >> configs/cherokee-${target}_defconfig
 make defconfig cherokee-${target}_defconfig
 make
